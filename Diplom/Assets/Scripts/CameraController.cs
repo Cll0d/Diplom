78b8372d5188a23d4d8002c7ea3ac4454689b3b4
doba26 <egsgdrz@mail.ru>
2023-10-24 18:38:24 +0500

    player and camera controller

---
+++ Diplom/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _sensivity;
+    private float _maxYAngel = 80f;
+
+    private float rotationX = 0;
+
+    private void Update()
+    {
+        ControllerCamera();
+    }
+
+    private void ControllerCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.parent.Rotate(Vector3.up * mouseX * _sensivity);
+
+        rotationX -= mouseY * _sensivity;
+        rotationX = Mathf.Clamp(rotationX, -_maxYAngel, _maxYAngel);
+        transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+    }
+}
--- Diplom/Assets/Scripts/PlayerController.cs
+++ Diplom/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed;
+    private CharacterController _characterController;
+
+    private void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Controller();
+    }
+
+    private void Controller()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+
+        moveDirection.y -= 9.81f * Time.deltaTime;
+
+        _characterController.Move(moveDirection * _moveSpeed * Time.deltaTime);
     }
 }
