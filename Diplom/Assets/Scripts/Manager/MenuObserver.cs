1b0f0a1793dd04e6f47f35166168ec4404c39801
doba26 <egsgdrz@mail.ru>
2024-04-01 16:37:32 +0500

    что то

--- Diplom/Assets/Scripts/Manager/MenuObserver.cs
+++ Diplom/Assets/Scripts/Manager/MenuObserver.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MenuObserver : MonoBehaviour
@@ -7,11 +8,11 @@ public class MenuObserver : MonoBehaviour
     [SerializeField] private GameObject _menu;
     [SerializeField] private PlayerController _playerController;
     [SerializeField] private CameraController _camertaContoller;
+    [SerializeField] private GameObject[] _podveska;
+    [SerializeField] private TMP_Text _textButton;
+    private bool _isVisible = false;
+    private bool _isVisiblePodveska = true;
 
-    private void Start()
-    {
-        _menu.SetActive(false);
-    }
 
     void Update()
     {
@@ -23,23 +24,57 @@ public class MenuObserver : MonoBehaviour
 
     private void ShowMenu()
     {
-        if (_menu.activeSelf == false)
+        if (_isVisible == false)
         {
-            _menu.SetActive(true);
-            _playerController.enabled = false;
-            _camertaContoller.enabled = false;
+
+            Pause(); 
         }
         else
         {
-            _menu.SetActive(false);
-            _playerController.enabled = true;
-            _camertaContoller.enabled = true;
+            Resume();
         }
     }
-    public void Continue()
+
+    private void Pause()
     {
-        _menu.SetActive(false);
-        _playerController.enabled = true;
+        _camertaContoller.enabled = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        _menu.SetActive(true);
+        _isVisible = true;
+        Time.timeScale = 0f;
+
+    }
+
+    public void Resume()
+    {
         _camertaContoller.enabled = true;
+        _menu.SetActive(false);
+        Time.timeScale = 1f;
+        _isVisible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ChangeDifficulty()
+    {
+        if (_isVisiblePodveska == true)
+        {
+            for (int i = 0; i < _podveska.Length; i++)
+            {
+                _podveska[i].GetComponent<MeshRenderer>().enabled = false;
+                _textButton.text = "Включить подсказку";
+                _isVisible = false;
+            }
+        }
+        if (_isVisiblePodveska == false)
+        {
+            for (int i = 0; i < _podveska.Length; i++)
+            {
+                _podveska[i].GetComponent<MeshRenderer>().enabled = true;
+                _textButton.text = "Выключить подсказку";
+                _isVisible = true;
+            }
+        }
     }
 }
